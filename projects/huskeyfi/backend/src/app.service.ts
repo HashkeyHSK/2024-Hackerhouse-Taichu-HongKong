fabd0930f10dce2fbc751f14aba01669e34ea59c
Suncheol Kwon <elytselyts@gmail.com>
2024-12-20 19:34:21 +0800

    feat: add createinvoice

--- projects/huskeyfi/backend/src/app.service.ts
+++ projects/huskeyfi/backend/src/app.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { Invoice, InvoiceResponse } from './types';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
+  private BTCPAY_URL = process.env.BTCPAY_URL;
+  private BRIDGE_CENTER_ID = process.env.BRIDGE_CENTER_ID;
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello Hashkey!!! 😄';
+  }
+
+  makeInvoice(amount: number): Invoice {
+    return {
+      amount: amount.toString(),
+      description: 'Bridge between Hashkey and Lightning Network',
+      descriptionHashOnly: false,
+      expiry: 600,
+      privateRouteHints: false,
+    };
+  }
+
+  // https://btcpay.stackstake.io/api/v1/stores/{storeId}/lightning/BTC/invoices
+  async createInvoice(amount: number): Promise<InvoiceResponse> {
+    const invoice = this.makeInvoice(amount);
+    console.log('creating invoice', invoice);
+    //   {
+    //     "id": "ecdf1c16f8ce8fe643fc9c6c76bfbef61d86a5afe5840ffbbc6cc939cbf7e542",
+    //     "status": "Unpaid",
+    //     "BOLT11": "lnbc8880n1pnk2n65pp5an03c9hce687vslun3k8d0a77cwcdfd0ukzql7audnynnjlhu4pqdqvdpshx6rtv4uscqzzsxqzz6sp509le7thnljsh0mkay4q3l6hxentwzs2v27nkpfscmmpqyxw7nf8s9qyyssqkx09jvv04a9jzwhyawt8xzkadh8qaj6dfk9863h4c3yx79m0y8zs9f65usutwyyv95tkkyvl78jj5r35qhzn4qkpthklyje76x9w9gsphm4mvt",
+    //     "paymentHash": "ecdf1c16f8ce8fe643fc9c6c76bfbef61d86a5afe5840ffbbc6cc939cbf7e542",
+    //     "paidAt": null,
+    //     "expiresAt": 1734692782,
+    //     "amount": "888000",
+    //     "amountReceived": null
+    // }
+
+    const response: InvoiceResponse = await axios.post(
+      `${this.BTCPAY_URL}/api/v1/stores/${this.BRIDGE_CENTER_ID}/lightning/BTC/invoices`,
+      invoice,
+    );
+    return response;
   }
 }
---
+++ projects/huskeyfi/backend/src/types.ts
@@ -0,0 +1,18 @@
+export interface Invoice {
+  amount: string;
+  description: string;
+  descriptionHashOnly: boolean;
+  expiry: number;
+  privateRouteHints: boolean;
+}
+
+export interface InvoiceResponse {
+  id: string;
+  status: string;
+  BOLT11: string;
+  paymentHash: string;
+  paidAt: string | null;
+  expiresAt: number;
+  amount: string;
+  amountReceived: string | null;
+}

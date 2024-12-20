79a30682ea2bb6b0824ac003f1ddab53737977ea
Nyangbari <hyojin.ju@gmx.net>
2024-12-20 20:50:43 +0800

    feat: number validation and wallet connect

--- projects/huskeyfi/frontend/app/_components/InputBox.tsx
+++ projects/huskeyfi/frontend/app/_components/InputBox.tsx
@@ -1,4 +1,40 @@
+"use client";
+
+import { useAppKit } from "@reown/appkit/react";
+import { useAccount, useBalance } from "wagmi";
+import { useEffect, useState } from "react";
+import { getBalance, GetBalanceReturnType } from "@wagmi/core";
+import { config } from "../config";
+import { HBTC } from "../constants/constants";
+
 const InputBox = () => {
+  const { open } = useAppKit();
+  const { address, isConnected } = useAccount();
+  const [tokenBalance, setTokenBalance] = useState<GetBalanceReturnType | null>(
+    null,
+  );
+  const [inputValue, setInputValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  useEffect(() => {
+    if (isConnected && address) {
+      getBalance(config, {
+        address,
+        token: HBTC,
+      }).then((balance) => setTokenBalance(balance));
+    }
+  }, [isConnected, address]);
+
+  const handleInputChange = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    const isValidNumber = /^\d*\.?\d*$/.test(value);
+    setIsValid(isValidNumber);
+    if (isValidNumber) {
+      setInputValue(value);
+    }
+  };
+
   return (
     <div className="flex w-full flex-col gap-5 rounded border border-huskey-gray-600 p-5">
       <div className="flex flex-col gap-4">
@@ -10,9 +46,14 @@ const InputBox = () => {
             inputMode="decimal"
             placeholder="0.0"
             className="w-full bg-transparent text-end outline-none"
+            value={inputValue}
+            onChange={handleInputChange}
           />
           <p className="ml-7">SAT</p>
         </div>
+        {!isValid && (
+          <p className="text-sm text-red-500">Please enter a valid number.</p>
+        )}
       </div>
       <div className="flex flex-col gap-4">
         <div className="flex items-end justify-between">
@@ -20,18 +61,34 @@ const InputBox = () => {
             Recipient Address <span className="text-sm">(HashKey Chain)</span>
           </p>
           <p className="text-sm font-normal text-huskey-gray-300">
-            Balance: 12345 SAT
+            Balance:{" "}
+            {!tokenBalance || tokenBalance.value === BigInt(0)
+              ? "0"
+              : tokenBalance.value.toString()}{" "}
+            SAT
           </p>
         </div>
-        <div className="flex w-full justify-center rounded border border-huskey-gray-600 p-4">
-          <p className="text-center">0xasdfj123h</p>
-        </div>
-        <button
-          type="button"
-          className="flex w-full justify-center rounded bg-huskey-primary-400 p-4 text-xl"
-        >
-          Send
-        </button>
+        {isConnected ? (
+          <>
+            <div className="flex w-full justify-center rounded border border-huskey-gray-600 p-4">
+              <p className="text-center">{address}</p>
+            </div>
+            <button
+              type="button"
+              className="flex w-full justify-center rounded bg-huskey-primary-400 p-4 text-xl"
+            >
+              Send
+            </button>
+          </>
+        ) : (
+          <button
+            type="button"
+            className="flex w-full justify-center rounded bg-huskey-primary-400 p-4 text-xl"
+            onClick={() => open()}
+          >
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );

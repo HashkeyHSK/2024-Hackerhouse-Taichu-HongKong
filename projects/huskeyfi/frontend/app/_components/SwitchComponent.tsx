fa87af5609de389c117eca7bf80fbe019b35b7d7
Hyunbin Lee <blackhblee@gmail.com>
2024-12-21 21:29:07 +0800

    fix: fix typo

--- projects/huskeyfi/frontend/app/_components/SwitchComponent.tsx
+++ projects/huskeyfi/frontend/app/_components/SwitchComponent.tsx
@@ -14,11 +14,11 @@ const SwitchComponent = () => {
   return (
     <div className="flex w-full items-center gap-2 text-2xl">
       <span className="text-huskey-gray-400">
-        {isToLN ? "HashKey" : "Lightning Network"}
+        {isToLN ? "HashKey Chain" : "Lightning Network"}
       </span>
       <ArrowRightIcon />
       <span className="text-huskey-primary-400">
-        {isToLN ? "Lightning Network" : "HashKey"}
+        {isToLN ? "Lightning Network" : "HashKey Chain"}
       </span>
       <div
         className="flex cursor-pointer items-center justify-center rounded border border-huskey-primary-400 p-1"
